59de32da8365fbd169bbacf1421d77b67a69b942
Anastashen <zvanast@gmail.com>
2023-05-28 19:46:06 +0300

    Initial commit

---
+++ task_2.py
@@ -0,0 +1,9 @@
+﻿from re import X
+
+
+S = int (input ('Введите число журавликов'))
+x = S // 6
+Petya = x
+Sergey = x
+Katya = 2 * (x + x)
+print (Petya, Katya, Sergey)
---
+++ task_3.py
@@ -0,0 +1,32 @@
+﻿# Вариант 1
+# n = input('Введите шестизначное число:')
+# oneNum = int (n[0] + n[1] + n[2])
+# oneSum = ((oneNum % 10) + ((oneNum // 10) % 10) + (oneNum // 100))
+# twoNum = int (n[3] + n[4] + n[5])
+# twoSum = ((twoNum % 10) + ((twoNum // 10) % 10) + (twoNum // 100))
+# if oneSum == twoSum:
+#     print('yes')
+# else:
+#     print('no')
+
+# Вариант 2
+n = input('Введите шестизначное число:')
+oneNum = int (n[0] + n[1] + n[2])
+oneSum = 0
+while oneNum > 0:
+    x = oneNum % 10
+    oneSum = oneSum + x
+    oneNum = oneNum // 10
+print (oneSum)
+
+twoNum = int(n[3] + n[4] + n[5])
+twoSum = 0
+while twoNum > 0:
+    x = twoNum % 10
+    twoSum = twoSum + x
+    twoNum = twoNum // 10
+print(twoSum)
+if oneSum == twoSum:
+    print('yes')
+else:
+    print('no')
